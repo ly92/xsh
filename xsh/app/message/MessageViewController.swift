841d9bda9458bf330a79ca34b113e61098b328cc
liyong <liyong@7xiaofu.com>
2018-12-21 14:41:56 +0800

    准备加入微信和支付宝

--- xsh/app/message/MessageViewController.swift
+++ xsh/app/message/MessageViewController.swift
@@ -13,7 +13,7 @@ class MessageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     //消息列表
