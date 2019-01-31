a1311b55881dc3f23a8661b7283494d8edcff887
liyong <liyong@7xiaofu.com>
2019-01-31 10:42:22 +0800

    order detail

--- xsh/app/personal/cells/order/OrderPayInfoCell.swift
+++ xsh/app/personal/cells/order/OrderPayInfoCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class OrderPayInfoCell: UITableViewCell {
     @IBOutlet weak var subView: UIView!
@@ -18,7 +19,7 @@ class OrderPayInfoCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.subView.layer.cornerRadius = 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -27,49 +28,21 @@ class OrderPayInfoCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    /**
-     "-----------URL--------"
-     "http://star.test.wwwcity.net/app/transaction/get?orderno=1901_6ea3bd6848a1978a8825bf900420&sign=7be48a633c02448f601dad77ba36c2ce&cmdno=bUQ6KfP4Kx0SQT859uuw1548863101&cid=6&ts=1548863101"
-     "-----------返回数据--------"
-     {
-     "code" : 0,
-     "message" : "",
-     "content" : {
-     "transaction" : {
-     "orderno" : "1901_6ea3bd6848a1978a8825bf900420",
-     "servicetype" : "商圈支付",
-     "atid" : "一卡通",
-     "status" : "已支付",
-     "totalprice" : "1.02",
-     "orderItems" : [
-     {
-     "unit" : "个",
-     "picurl" : "http:\/\/starlife3c.test.upcdn.net\/image\/201901\/1547110391729.jpg",
-     "price" : "1.02",
-     "oid" : 196,
-     "title" : "测试商品",
-     "totalprice" : "1.02",
-     "pid" : 10096,
-     "amount" : 1
-     }
-     ],
-     "creationtime" : "2019-01-17 11:45:48",
-     "memo" : "商圈支付",
-     "money" : "0.00",
-     "rejecttime" : "1970-01-01 08:00:00",
-     "amount" : 1
-     },
-     "points" : {
-     "money" : "1.02",
-     "points" : "1.02"
-     },
-     "coupon" : [
-     
-     ]
-     },
-     "contentEncrypt" : ""
-     }
-
-     */
-    
+    var subJson = JSON(){
+        didSet{
+            var pointM : Float = 0
+            for point in self.subJson["points"].arrayValue{
+                pointM += point["money"].floatValue
+            }
+            
+            var couponM : Float = 0
+            for coupon in self.subJson["coupon"].arrayValue{
+                couponM += coupon["money"].floatValue
+            }
+            
+            self.couponLbl.text = String.init(format: "-%.2f", couponM)
+            self.pointLbl.text = String.init(format: "-%.2f", pointM)
+            self.totalMoneyLbl.text = self.subJson["money"].stringValue
+        }
+    }
 }
--- xsh/app/personal/views/OrderDetailViewController.swift
+++ xsh/app/personal/views/OrderDetailViewController.swift
@@ -7,22 +7,38 @@
 //
 
 import UIKit
+import SwiftyJSON
+
 
 class OrderDetailViewController: BaseTableViewController {
 
     var orderno = ""
     
+    fileprivate var goodsList : Array<JSON> = []
+    fileprivate var couponList : Array<JSON> = []
+    fileprivate var orderInfo = JSON()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.backgroundColor = BG_Color
         self.tableView.separatorStyle = .none
+        self.tableView.register(UINib.init(nibName: "OrderDetailCell", bundle: Bundle.main), forCellReuseIdentifier: "OrderDetailCell")
+        self.tableView.register(UINib.init(nibName: "OrderGoodsCell", bundle: Bundle.main), forCellReuseIdentifier: "OrderGoodsCell")
+        self.tableView.register(UINib.init(nibName: "OrderPayInfoCell", bundle: Bundle.main), forCellReuseIdentifier: "OrderPayInfoCell")
+        self.tableView.register(UINib.init(nibName: "CouponCell", bundle: Bundle.main), forCellReuseIdentifier: "CouponCell")
+        
+        
+        self.loadDetailData()
     }
 
     //加载详情
     func loadDetailData() {
         let params = ["orderno" : self.orderno]
         NetTools.requestData(type: .post, urlString: CardTransactionDetailApi, parameters: params, succeed: { (result) in
-            
+            self.orderInfo = result
+            self.goodsList = result["transaction"]["orderItems"].arrayValue
+            self.couponList = result["coupon"].arrayValue
+            self.tableView.reloadData()
         }) { (error) in
             LYProgressHUD.showError(error)
         }
@@ -30,70 +46,101 @@ class OrderDetailViewController: BaseTableViewController {
     
     
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 4
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if section == 0 || section == 2{
+            return 1
+        }else if section == 1{
+            return self.goodsList.count
+        }else if section == 3{
+            return self.couponList.count
+        }
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        if indexPath.section == 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "OrderDetailCell", for: indexPath) as! OrderDetailCell
+            cell.subJson = self.orderInfo
+            return cell
+        }else if indexPath.section == 1{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "OrderGoodsCell", for: indexPath) as! OrderGoodsCell
+            if self.goodsList.count > indexPath.row{
+                let json = self.goodsList[indexPath.row]
+                cell.subJson = json
+            }
+            return cell
+        }else if indexPath.section == 2{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "OrderPayInfoCell", for: indexPath) as! OrderPayInfoCell
+            cell.subJson = self.orderInfo
+            return cell
+        }else if indexPath.section == 3{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CouponCell", for: indexPath) as! CouponCell
+            if self.couponList.count > indexPath.row{
+                let json = self.couponList[indexPath.row]
+                cell.subJson = json
+                cell.timeLbl.text = "已使用"
+            }
+            return cell
+        }
+        return UITableViewCell()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0{
+            return 200
+        }else if indexPath.section == 1{
+            return 60
+        }else if indexPath.section == 2{
+            return 135
+        }else if indexPath.section == 3{
+            return 120
+        }
+        return 0
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 || section == 2{
+            return nil
+        }
+        
+        let view = UIView.init(frame: CGRect.init(x: 0, y: 0, width: kScreenW, height: 50))
+        view.backgroundColor = BG_Color
+        let subView = UIView.init(frame: CGRect.init(x: 10, y: 8, width: kScreenW - 20, height: 42))
+        subView.backgroundColor = UIColor.white
+        view.addSubview(subView)
+        let lbl = UILabel.init(frame: CGRect.init(x: 8, y: 10, width: kScreenW - 36, height: 21))
+        lbl.font = UIFont.systemFont(ofSize: 14.0)
+        lbl.textColor = UIColor.colorHex(hex: "666666")
+        
+        if section == 1{
+            lbl.text = "商品信息"
+            if self.goodsList.count > 0{
+                return view
+            }
+        }else if section == 3{
+            lbl.text = "优惠券信息"
+            if self.couponList.count > 0{
+                return view
+            }
+            return view
+        }
+        return nil
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 1{
+            if self.goodsList.count > 0{
+                return 50
+            }
+        }else if section == 3{
+            if self.couponList.count > 0{
+                return 50
+            }
+        }
+        return 0.001
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
