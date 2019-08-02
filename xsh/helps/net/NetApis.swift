21cf5dc58f5ca4e2946876d2ae314b9c038a88b0
ly <1364757394@qq.com>
2019-08-02 09:37:00 +0800

    motion

--- xsh/helps/net/NetApis.swift
+++ xsh/helps/net/NetApis.swift
@@ -18,8 +18,8 @@ let officialServer = "http://star.wwwcity.net/app/"//正式服务器
 let testServer = "http://star.test.wwwcity.net/app/"//测试服务器
 //let testServer = "http://star.pos.wwwcity.net/app/"//测试服务器
 let testServer2 = "http://xing.com/app/"//测试服务器
-//let usedServer = officialServer
-let usedServer = testServer
+let usedServer = officialServer
+//let usedServer = testServer
 let DeBug = false
 
 
